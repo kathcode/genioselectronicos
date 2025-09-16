d8547e8ba2f66459523c8d2142e32b282711963c
mountechcorp <mountechcorp@gmail.com>
2025-09-15 22:00:15 -0400

    update banner image

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,7 +6,7 @@ export function Banner() {
     <section id="inicio" className="relative">
       <div className="relative h-[68vh] w-full">
         <Image
-          src="/banner.png"
+          src="https://github.com/kathcode/genioselectronicos/blob/main/public/banner.png"
           alt="Equipo creativo trabajando con dispositivos electrónicos"
           fill
           priority
